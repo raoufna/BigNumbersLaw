a17203f7212c8fc37c6de3ccfe8ac1723514cfd4
Raouf Namane <rnamane17@gmail.com>
2025-02-03 16:09:34 +0100

    semantic adjust

--- BigNumberLaw.java
+++ BigNumberLaw.java
@@ -11,13 +11,14 @@ class BigNumberLaw{
   
     public static void main(String args[])
     {   
-        //THEORY: If I roll many times, the average will be close 3.5 
+        //THEORY: The average value of a fair 6-sided die is 3.5.
+        //If I roll many times, the average will be close 3.5 
         double average = 0;
-        long many = 100000;
-        for(int i = 0; i < n; i++){
+        long many = 10000000;
+        for(int i = 0; i < many; i++){
             average += die();
         }
-        average /= n;
+        average /= many;
         System.out.println("The average is: " + average);
         System.out.println("distance: "+ Math.abs(3.5 - average));
     }
